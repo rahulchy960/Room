bc98d56037f532575ed6c8940fb77e7cc104db54
rahulchy960 <rahulchy960@gmail.com>
2025-10-31 04:55:20 +0530

    02: Theme

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-800 font-sans dark:bg-black">
+      <p className=" text-3xl text-orange-500 ">
+        Room Chat
+      </p>
+    </div>
+  );
+}
