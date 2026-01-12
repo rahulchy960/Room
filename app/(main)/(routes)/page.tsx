509e86a40ca6395fd46f58586ff45cdebf382ff5
rahulchy960 <rahulchy960@gmail.com>
2026-01-13 01:53:06 +0530

    03: Authentication via Clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-800 font-sans dark:bg-black">
-      <p className=" text-3xl text-orange-500 ">
-        Room Chat
+      <p className=" text-3xl text-indigo-500 ">
+        Room Chat: This is a Protected Route
+      </p>
+      <p>
+        <UserButton />
       </p>
     </div>
   );
