d54b7bea602ae761c174900ce6e3241cecdf4553
rahulchy960 <rahulchy960@gmail.com>
2026-03-16 02:04:34 +0530

    15: Channel Id Page

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,8 +1,53 @@
-const ServerIdPage = () => {
+import { currentProfile } from "@/lib/current-profile";
+import { prisma } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+interface ServerIdPageProps {
+  params: Promise<{
+    serverId: string;
+  }>;
+}
+
+
+const ServerIdPage = async ({
+  params
+}: ServerIdPageProps) => {
+
+  const { serverId } = await params;
+  const profile = await currentProfile();
+
+  if (!profile) {
+      redirect("/sign-in");
+  }
+
+  const server = await prisma.server.findUnique({
+    where: {
+      id: serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        }
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: 'General'
+        },
+        orderBy: {
+          createdAt: "asc"
+        }
+      }
+    }
+  });
+
+  const initialChannel = server?.channels[0];
+  if(initialChannel?.name !== "General") {
+    return null;
+  }
+
   return (
-    <div>
-      Server Id Page
-    </div>
+    redirect(`/servers/${serverId}/channels/${initialChannel?.id}`)
   )
 }
 
